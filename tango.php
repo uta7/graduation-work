456354a399e8685e6aa3bb83d57586e7a6051605
uta7 <122001807+uta7@users.noreply.github.com>
2023-12-15 14:57:59 +0900

    Add files via upload

--- tango.php
+++ tango.php
@@ -1,6 +1,6 @@
   <button id="testStartBtn" type="button">テスト開始</button>
-  <div>
-    <p>単語一覧</p>
+  <div class="flexPa" >
+    <p class="marginChi" >単語一覧</p>
     <p>検索</p>
   </div>
   <div class="radios" >
@@ -9,31 +9,40 @@
     <input id="hideJp" class="radio" type="radio" name="a">日本語を隠す
   </div>
 
-  <table>
-    <tr>
-      <th>苦手度</th>
-      <th>英語</th>
-      <th>日本語</th>
-    </tr>
-    <tr>
-      <td class="td" >
-        <div class="flex">
-          <div class="left">
-            <!-- 苦手度上昇ボタン -->
-            <button class="badBtn" id="up" type="button">▲</button>
-            <!-- 苦手度下降ボタン --><br>
-            <button class="badBtn" id="down" type="button">▼</button>
-          </div>
-          <p id="bad"><p>
-        </div>
-      </td>
-      <td class="td" id="Eng">sample</td>
-      <td class="td" id="Jp">サンプル</td>
-    </tr>
-  </table>
+      <?php
+        $table = array(
+          array("a1", "a2"),
+          array("b1", "b2"),
+        );
+      ?>
+        <table>
+          <thead>
+            <tr><th>苦手度</th><th>英語</th><th>日本語</th></tr>
+          <thead>
+        <tbody>
+        <?php foreach($table as $row): ?>
+          <tr>
+            <td class="td" >
+              <div class="flex">
+                <div class="left">
+                  <!-- 苦手度上昇ボタン -->
+                  <button class="badBtn" id="up" type="button">▲</button>
+                  <!-- 苦手度下降ボタン --><br>
+                  <button class="badBtn" id="down" type="button">▼</button>
+                </div>
+                <p class="bad"><p>
+              </div>
+            </td>
+            <?php foreach($row as $cel): ?>
+              <td><?= $cel ?></td>
+            <?php endforeach; ?>
+          </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
 
   <script>
-    var bad = document.getElementById("bad");
+    var bad = document.getElementsByClassName("bad");
     const up = document.getElementById('up');
     const down = document.getElementById('down');
     bad.textContent = 0;
@@ -44,5 +53,6 @@
       bad.textContent--;
     });
 
-
   </script>
+
+
